b80314eb9b016e6910afa7aed8a78a5613d20495
Dao Yen <yenn.vcu@gmail.com>
2025-03-17 10:44:15 +0700

    Initial commit

---
+++ actions/commons/GlobalConstants.java
@@ -0,0 +1,6 @@
+package commons;
+
+public class GlobalConstants {
+    // User/Pass
+    // Url
+}
---
+++ testcases/com/nopecommerce/users/Level_01_Register.java
@@ -0,0 +1,28 @@
+package com.nopecommerce.users;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Level_01_Register {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+    }
+    @Test
+    public void Register_01(){
+
+    }
+    @Test
+    public void Register_02(){
+
+    }
+    @AfterClass
+    public void afterClass() {
+
+    }
+}
